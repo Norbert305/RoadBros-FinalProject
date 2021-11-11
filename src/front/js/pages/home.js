674e009ad61102db3977f7a2e2de4edd2a685aa9
Rcodeboss <morejonrafa@gmail.com>
2021-11-11 00:48:41 +0000

    rafa

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,7 +8,7 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Welcome to RoadBros! Paola</h1>
+			<h1>Welcome to RoadBros! Rafa</h1>
 			<p>
 				<img
 					src={
