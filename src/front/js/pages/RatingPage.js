edbd959c9c200078b9bd598c9762dddc37610cb1
Norbert305 <sealsnorbert@yahoo.com>
2021-11-21 15:50:32 +0000

    rating page changes Norbert

--- src/front/js/pages/RatingPage.js
+++ src/front/js/pages/RatingPage.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-
+import { Button, Modal } from "react-bootstrap";
 import { Context } from "../store/appContext";
 
 import "../../styles/demo.scss";
 
 export const RatingPage = () => {
 	const { store, actions } = useContext(Context);
+	const [showUp, setShowUp] = useState(false);
+
+	const handleCloseUp = () => setShowUp(false);
+	const handleShowUp = () => setShowUp(true);
+
+	const [showDown, setShowDown] = useState(false);
+
+	const handleCloseDown = () => setShowDown(false);
+	const handleShowDown = () => setShowDown(true);
 
 	const [newRating, setnewRating] = useState({
 		thumbUp: null,
@@ -21,30 +30,46 @@ export const RatingPage = () => {
 			<h5 className="m-5">How would you rate your experience with ___________ ?</h5>
 
 			<div className="row justify-content-center m-3 ">
-				<button
-					type="button"
-					className="btn btn-light col"
-					onClick={() =>
-						alert(
-							"Thank you for your feed back! You will recieve an notifcation email in regards to todays service! Please fill out our comment section below on how we can improve! "
-						)
-					}>
+				<button type="button" className="btn btn-light col" onClick={handleShowUp}>
 					<div className="col">
 						<i className="fas fa-thumbs-up fa-4x" />
 					</div>
 				</button>
-				<button
-					type="button"
-					className="btn btn-light col"
-					onClick={() =>
-						alert(
-							"We apoligize! Sorry for the inconvenience! Please fill out our comment section below on how we can improve!"
-						)
-					}>
+				<Modal show={showUp} onHide={handleCloseUp}>
+					<Modal.Header closeButton>
+						<Modal.Title>Modal heading</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						Thanks for your feedback! Please fill out form bellow on how we can improve!
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={handleCloseUp} className="btn-warning">
+							Close
+						</Button>
+						<Button variant="primary" onClick={handleCloseUp}>
+							Save Changes
+						</Button>
+					</Modal.Footer>
+				</Modal>
+				<button type="button" className="btn btn-light col" onClick={handleShowDown}>
 					<div className="col">
 						<i className="fas fa-thumbs-down fa-4x" />
 					</div>
 				</button>
+				<Modal show={showDown} onHide={handleCloseDown}>
+					<Modal.Header closeButton>
+						<Modal.Title>Modal heading</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>Sorry for the inconvenience! Please feel free to fill out our form bellow.</Modal.Body>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={handleCloseDown} className="btn-warning">
+							Close
+						</Button>
+						<Button variant="primary" onClick={handleCloseDown}>
+							Save Changes
+						</Button>
+					</Modal.Footer>
+				</Modal>
 			</div>
 
 			<div className="mt-5 p-3">
@@ -64,10 +89,10 @@ export const RatingPage = () => {
 				<button
 					className="btn btn-warning btn-lg mt-4"
 					/*
-					onClick={() => {
-						actions.addRating(newRating);
-					}}
-					*/
+				onClick={() => {
+					actions.addRating(newRating);
+				}}
+				*/
 				>
 					Send Rating
 				</button>
