27827450057c1a70427e3b1e4a99f69874d1d756
Rcodeboss <morejonrafa@gmail.com>
2021-12-01 23:46:55 +0000

    home style

--- src/front/js/pages/ClientHomePage.js
+++ src/front/js/pages/ClientHomePage.js
@@ -11,20 +11,19 @@ export const ClientHomePage = () => {
 	return (
 		<div className="container py-4 px-3 text-center text-light fs-4">
 			<h1 className="text-center my-5">Home Page</h1>
-
-			<div className="row my-3 d-flex justify-content-center">
-				<div className="col-4 bg-light text-dark p-3 rounded-3">
-					<div>
-						<h5>No request at the time</h5>
-					</div>
-				</div>
-			</div>
 			<div className="row my-3 d-flex flex-column justify-content-center">
-				<Link to="/ClientProfile">
-					<button className="btn btn-warning btn-lg p-2 m-3">Profile</button>
-				</Link>
 				<Link to="/ServiceMenu">
 					<button className="btn btn-warning btn-lg p-2 m-3">Request Help</button>
+				</Link>
+				<div className="row my-3 d-flex justify-content-center">
+					<div className="col-4 bg-light text-dark p-3 rounded-3">
+						<div>
+							<h5>No request at the time</h5>
+						</div>
+					</div>
+				</div>
+				<Link to="/ClientProfile">
+					<button className="btn btn-warning btn-lg p-2 m-3">Profile</button>
 				</Link>
 				<Link to="/ServiceHistory">
 					<button className="btn btn-warning btn-lg m-3">Service History</button>
